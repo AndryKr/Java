b2f7fb3201313d5fa22ec4257cb4aefeb6339c16
AndryKr <yandryx@ya.ru>
2018-10-15 00:56:41 +0300

    Поправка для первого условия: 1. Пароль должен быть не менее 8ми символов.

--- CheckPassword.java
+++ CheckPassword.java
@@ -18,7 +18,7 @@ public class CheckPassword {
     }
 
     static boolean isValid(String line) {
-        if (line.length()==8) {
+        if (line.length()>=8) {
             String pattern = "(.*[a-z]+.*[0-9]+.*[A-Z]+.*)|(.*[0-9]+.*[a-z]+.*[A-Z]+.*)" +
                     "|(.*[A-Z]+.*[0-9]+.*[a-z]+.*)|(.*[A-Z]+.*[a-z]+.*[0-9]+.*)|" +
                     "(.*[a-z]+.*[A-Z]+.*[0-9]+.*)|(.*[0-9]+.*[A-Z]+.*[a-z]+.*)";
