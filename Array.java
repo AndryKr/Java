44c37876192558e3858aca95cd9f50c0a58f1e1e
AndryKr <yandryx@ya.ru>
2018-11-26 01:40:48 +0300

    Решение домашнего задания ко второму уроку

---
+++ Array.java
@@ -0,0 +1,21 @@
+/**
+ * Java. Alg. Lesson 2. Homework
+ *
+ * @author Andry Krasikov
+ * @version Nov 26, 2018
+ */
+
+public interface Array {
+
+    void add(int value);
+
+    boolean remove(int value);
+
+    boolean find(int value);
+
+    void sortBubble();
+
+    void sortSelect();
+
+    void sortInsert();
+}
