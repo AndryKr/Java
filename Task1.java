5bb535486752218aeb23e63801774ac5b815400e
Andry <41024438+AndryKr@users.noreply.github.com>
2020-11-02 13:01:59 +0300

    Update Task1.java
    Переименовал метод

--- Task1.java
+++ Task1.java
@@ -1,6 +1,6 @@
 public class Task1 {
 
-    public static <T> T[] arrToList(T[] arr, int index1, int index2) {
+    public static <T> T[] swapTwoElementsOfArray(T[] arr, int index1, int index2) {
         T variable  = arr[index1];
         arr[index1] = arr [index2];
         arr [index2] = variable;
