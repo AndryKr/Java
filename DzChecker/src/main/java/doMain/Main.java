6542b73ae27b1f6fc0509ba3b155e392f6f370b0
AndryKr <yandryx@ya.ru>
2020-11-26 04:36:27 +0300

    Решение тестовой задачи - заполнение матрицы по спирали.

--- DzChecker/src/main/java/doMain/Main.java
+++ DzChecker/src/main/java/doMain/Main.java
@@ -3,6 +3,7 @@ package doMain;
 import services.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
         String path = "";
         Scanner.getInstance().findClass(path);
